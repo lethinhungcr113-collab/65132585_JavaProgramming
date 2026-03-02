cdeccd7e0a8a39acef9476419e7ab36c5714a861
lethinhungcr113-collab <lethinhungcr113@gmail.com>
2026-03-02 23:43:36 +0700

    delta

---
+++ Lab1_Bai4_Delta/src/delta.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class delta {
+
+	public static void main(String[] args) {
+		Scanner heso = new Scanner (System.in);
+
+		System.out.print("Nhập hệ số a: ");
+		double a = heso.nextDouble();
+		
+		System.out.print("Nhập hệ số b: ");
+		double b = heso.nextDouble();
+		
+		System.out.print("Nhập hệ số c: ");
+		double c = heso.nextDouble();
+		
+		double delta = Math.pow(b, 2)-4*a*c;
+		
+		System.out.println("Delta = " + delta);
+		
+		if (delta >= 0) {
+            double canDelta = Math.sqrt(delta);
+            System.out.println("Căn delta = " + canDelta);
+        } else {
+            System.out.println("Delta âm, không có căn bậc hai thực.");
+        }
+
+        heso.close();
+	}
+
+}
